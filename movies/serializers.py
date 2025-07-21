fef567e8840f11f1946741a9d684ce81ddb92f6b
amangulov03 <tilekuulue@gmail.com>
2025-07-21 20:31:14 +0600

    search_felter

--- movies/serializers.py
+++ movies/serializers.py
@@ -8,8 +8,15 @@ class GenreSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class MovieSerializer(serializers.ModelSerializer):
-    genres = GenreSerializer(many=True)
+    genres = GenreSerializer(many=True, read_only=True)
 
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def get_fields(self):
+        fields = super().get_fields()
+        request = self.context.get('request')
+        if not request:
+            fields.pop('video')
+        return fields
