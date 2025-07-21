ad1fc5700d2677d16809cf72dda8add69eec4ade
Ken95is <isken.asanov@gmail.com>
2025-07-21 19:15:23 +0600

    models_movie

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Movie, Genre
+
+@admin.register(Movie)
+class MoviesAdmin(admin.ModelAdmin):
+    filter_horizontal = ('genres',)
+
+admin.site.register(Genre)
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    year = models.PositiveIntegerField()
+    genres = models.ManyToManyField(Genre)
+    poster = models.ImageField(upload_to='posters/', blank=True)
+    video = models.FileField(upload_to='video/', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
